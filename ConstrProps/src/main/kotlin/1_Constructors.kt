0608a6fbafb1483c80b27ffa8031c7232d668c61
Pedro Pereira <palex@cc.isel.ipl.pt>
2022-11-10 18:14:36 +0000

    Aula 10 Novembro
    Properties

---
+++ ConstrProps/src/main/kotlin/1_Constructors.kt
@@ -0,0 +1,23 @@
+
+class Student1 {
+    val number: Int
+    val name: String
+
+    init { print("Init -> ") }
+    constructor(n: Int, id:String) {
+        print("C(n,id) -> ")
+        number = n
+        name = id
+    }
+    constructor() {
+        print("C() -> ")
+        number = 0
+        name =""
+    }
+    fun println() = println("$number - ($name)")
+}
+
+fun main() {
+    Student1().println()
+    Student1(48000,"Pedro Pereira").println()
+}
---
+++ ConstrProps/src/main/kotlin/5_PropsPrimary.kt
@@ -0,0 +1,13 @@
+
+class Student5(val number: Int=0, val name:String="") {
+    init { print("Init -> ") }
+    //constructor() : this(0,"")
+    fun println() = println("$number - ($name)")
+}
+
+fun main() {
+    Student5().println()
+    Student5(48000,"Pedro Pereira").println()
+    Student5(47000).println()
+    Student5(name="Ana Paula").println()
+}
